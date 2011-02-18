65eeea05d07d4019594effd780f1456055f5988e
Matias Bjørling <MatiasBjorling@gmail.com>
2011-02-18 17:53:21 +0100

    Added sequential testcase.

--- run_test.cpp
+++ run_test.cpp
@@ -40,10 +40,30 @@ int main()
 
 	double result;
 
+//	// Test one write to some blocks.
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		/* event_arrive(event_type, logical_address, size, start_time) */
+//		result = ssd -> event_arrive(WRITE, i*100000, 1, (double) 1+(250*i));
+//
+//		printf("Write time: %.20lf\n", result);
+////		result = ssd -> event_arrive(WRITE, i+10240, 1, (double) 1);
+////		printf("Write time: %.20lf\n", result);
+//	}
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		/* event_arrive(event_type, logical_address, size, start_time) */
+//		result = ssd -> event_arrive(READ, i*100000, 1, (double) 1+(500*i));
+//		printf("Read time : %.20lf\n", result);
+////		result = ssd -> event_arrive(READ, i, 1, (double) 1);
+////		printf("Read time : %.20lf\n", result);
+//	}
+
+	// Test writes and read to same block.
 	for (int i = 0; i < SIZE; i++)
 	{
 		/* event_arrive(event_type, logical_address, size, start_time) */
-		result = ssd -> event_arrive(WRITE, i*100, 1, (double) 1+(250*i));
+		result = ssd -> event_arrive(WRITE, i, 1, (double) 1+(250*i));
 
 		printf("Write time: %.20lf\n", result);
 //		result = ssd -> event_arrive(WRITE, i+10240, 1, (double) 1);
@@ -52,7 +72,7 @@ int main()
 	for (int i = 0; i < SIZE; i++)
 	{
 		/* event_arrive(event_type, logical_address, size, start_time) */
-		result = ssd -> event_arrive(READ, i*100, 1, (double) 1);
+		result = ssd -> event_arrive(READ, i, 1, (double) 1+(500*i));
 		printf("Read time : %.20lf\n", result);
 //		result = ssd -> event_arrive(READ, i, 1, (double) 1);
 //		printf("Read time : %.20lf\n", result);
