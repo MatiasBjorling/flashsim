fd55d996fe422586d89d690e644afcfbcbbbc53d
Matias Bjørling <MatiasBjorling@gmail.com>
2011-03-22 11:03:40 +0100

    DFTL file

---
+++ FTLs/dftl_ftl.cpp
@@ -0,0 +1,26 @@
+/* Copyright 2011 Matias Bjørling */
+
+/* dftp_ftl.cpp  */
+
+/* FlashSim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version. */
+
+/* FlashSim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License
+ * along with FlashSim.  If not, see <http://www.gnu.org/licenses/>. */
+
+/****************************************************************************/
+
+/* Implementation of the DFTL described in the paper
+ * "DFTL: A Flasg Translation Layer Employing Demand-based Selective Caching og Page-level Address Mappings"
+ *
+ * Notice: Startup procedures are not implemented as the drive is empty every time
+ * the simulator is executed. i.e. OOB's is not filled with logical page address
+ * at write and it is not read on startup to recreate mapping tables.
+ */
